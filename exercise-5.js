a1f9a2596e792a9b5ab3b09945d4a5fd14c8f300
Syarief A <syariefarifin107@gmail.com>
2019-08-08 03:37:54 +0700

    latihan 5

--- exercise-5.js
+++ exercise-5.js
@@ -1,2 +1,16 @@
-var js = [1, 1, 1];
-console.log(js.indexOf('1');
+function ubahHuruf(kata) {
+    // you can only write your code here!
+    var kataBaru = '';
+
+    for(var m = 0; m < kata.length; m++){
+      kataBaru = kataBaru + String.fromCharCode(kata.charCodeAt(m) + 1);
+    }
+    return kataBaru;
+  }
+  
+  // TEST CASES
+  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('developer')); // efwfmpqfs
+  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  console.log(ubahHuruf('keren')); // lfsfo
+  console.log(ubahHuruf('semangat')); // tfnbohbu
